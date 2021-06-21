9e778f66b3f17bfd781518b4de05cc4c53633ead
Pedro Castro <85654674+PedroCastro-999@users.noreply.github.com>
2021-06-21 01:23:07 -0300

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,12 @@
-export const CHANNEL_NAME = '#gabsbmartins';
-export const API_RIOT_KEY = 'RGAPI-4e576a99-53c2-432f-a525-ec37a6b4d5c0'
+export const CHANNEL_NAME = '#xxxxxxxxxxxxs';
+export const API_RIOT_KEY = 'xxxxxxxxxxxxxxxxxxxx'
 export const ID_GA = '8wcvJ1LN6hLBgBlnUHR9ah2hiVntMKjS7s1plG84Pz-pHzU'
-export const OAUTH_TOKEN = 'oauth:xbujp2uu2pgnnbf2xiemsslx1k6udq';
-export const BOT_USERNAME = 'KyaraBot';
+export const OAUTH_TOKEN = 'oxxxxxxxxxxxxxxxxxxxxxxx;
+export const BOT_USERNAME = 'xxxxxxxxxxxxxxxxxxxxxxxxxxx';
 export const BLOCKED_WORDS = [
-   'Vadia', 'Puta', 'piranha', 'vaca', 'bolsonaro', ' ⣇⢸⢸⣿⡿⢸⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⢟⢂⡇⡟⢼⠟ '
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+  
 ]
