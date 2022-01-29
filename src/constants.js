77f1b620d1dafc49ad413778ec4206eb9959e64f
Pedro Castro <85654674+PedroCastro-999@users.noreply.github.com>
2022-01-29 00:13:04 -0300

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const CHANNEL_NAME = '#xxxxxxxxxxxxs';
 export const API_RIOT_KEY = 'xxxxxxxxxxxxxxxxxxxx'
-export const ID_GA = '8wcvJ1LN6hLBgBlnUHR9ah2hiVntMKjS7s1plG84Pz-pHzU'
+export const ID_GA = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 export const OAUTH_TOKEN = 'oxxxxxxxxxxxxxxxxxxxxxxx;
 export const BOT_USERNAME = 'xxxxxxxxxxxxxxxxxxxxxxxxxxx';
 export const BLOCKED_WORDS = [
